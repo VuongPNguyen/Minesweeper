1b8c8749f5c1b83f2b1fb14cade780dd80a75069
Vuong Nguyen <vpn@unc.edu>
2025-02-07 13:54:46 -0500

    Setup

---
+++ src/main/java/org/example/view/FXComponent.java
@@ -0,0 +1,8 @@
+package org.example.view;
+
+import javafx.scene.Parent;
+
+public interface FXComponent {
+  /** Render the component and return the resulting Parent object */
+  Parent render();
+}
